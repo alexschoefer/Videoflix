e14a2bdb96c6abe67d9169f93b3d9daac6ffb072
alexschoefer <alex.schoefer@gmx.net>
2026-05-04 21:06:53 +0200

    feat: register video_app for admin console; add video_file in model video

--- video_app/admin.py
+++ video_app/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from video_app.models import Video
 
 # Register your models here.
+@admin.register(Video)
+class VideoAdmin(admin.ModelAdmin):
+    """
+    Admin interface for the Video model.
+    This class defines the configuration for how the Video model is displayed and managed in the Django admin interface.
+     Attributes:
+        list_display (tuple): A tuple of field names to be displayed in the list view of the admin interface.
+        search_fields (tuple): A tuple of field names that can be searched using the search box in the admin interface.
+        ordering (tuple): A tuple defining the default ordering of the video records in the admin interface, in this case, ordered by creation date in descending order.
+    """
+    list_display = ('id', 'title', 'created_at')
+    search_fields = ('title',)
+    ordering = ('-created_at',)
--- video_app/signals.py
+++ video_app/signals.py
@@ -2,15 +2,42 @@ from django.db.models.signals import post_save, post_delete
 from .models import Video
 from django.dispatch import receiver
 import os
+from .tasks import convert_video_to_hls
 
+"""
+Signal handler for the post_save signal of the Video model.
+"""
 @receiver(post_save, sender=Video)
 def video_post_save(sender, instance, created, **kwargs):
+    """
+    Signal handler for the post_save signal of the Video model. 
+    This function is triggered whenever a Video instance is saved. 
+    If the instance is newly created, it enqueues a task to convert the video to HLS format and create a thumbnail using Django RQ.
+    Args:
+        sender (Model): The model class that sent the signal.
+        instance (Video): The instance of the Video model that was saved.
+        created (bool): A boolean indicating whether the instance was created (True) or updated (False).
+        **kwargs: Additional keyword arguments passed by the signal.
+    """
     if created:
         import django_rq
         queue = django_rq.get_queue('default')
+        queue.enqueue(convert_video_to_hls, instance.id)
 
+"""
+Signal handler for the post_delete signal of the Video model.
+"""
 @receiver(post_delete, sender=Video)
 def video_post_delete(sender, instance, **kwargs):
+    """
+    Signal handler for the post_delete signal of the Video model.
+    This function is triggered whenever a Video instance is deleted.
+    It checks if the video file and thumbnail associated with the deleted instance exist on the filesystem, and if they do, it removes them to free up storage space.
+    Args:
+    sender (Model): The model class that sent the signal.
+    instance (Video): The instance of the Video model that was deleted.
+    **kwargs: Additional keyword arguments passed by the signal.
+    """
     if instance.video_file:
         if os.path.isfile(instance.video_file.path):
             os.remove(instance.video_file.path)
